5f202844b27db1d2ff968e3192fd2001b24c2e32
JIICE <minotjeanclaude60@gmail.com>
2024-06-05 13:31:13 +0100

    Code the routing system

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProductComponent } from './product/product.component';
+
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "a-propos", component: AboutComponent },
+  { path: "contact", component: ContactComponent },
+  { path: "produit", component: ProductComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
