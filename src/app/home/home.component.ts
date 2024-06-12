04044c60fe8d9c37b1669671aeadb23d9467a5f4
JIICE <minotjeanclaude60@gmail.com>
2024-06-12 17:50:02 +0100

    Allow enabling and disabling ascending and descending sorting

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,23 +8,45 @@ import { Card } from '../card/card.model';
   styleUrl: './home.component.css'
 })
 export class HomeComponent implements OnInit {
-  // Le type de la propriété "card" correspond au tableau "Card[]"
+  // Le type de la propriété "cards" correspond au tableau "Card[]"
   // provenant du fichier "card.model.ts".
   cards: Card[] = [];
-
+  originalCards: Card[] = [];
 
   searchText: string = '';
 
   isClickedAsc: boolean = false;
   isClickedDesc: boolean = false;
 
+  ascClickCount: number = 0;
+  descClickCount: number = 0;
+
+  // L'utilisation du modulo permet d'activer tel ordre lorsque
+  // l'utilisateur clique une première fois et de le désactiver 
+  // lorsqu'il re-clique sur le même ordre.
   toggleSortOrder(order: string): void {
     if (order === 'asc') {
-      this.isClickedAsc = true;
-      this.isClickedDesc = false;
+      this.ascClickCount++;
+      if(this.isClickedAsc = this.ascClickCount % 2 !== 0) {
+        this.isClickedAsc = true;
+        this.isClickedDesc = false;
+        console.log('Ordre croissant activé(nombre de clic impair)');
+      } else {
+        this.isClickedAsc = false;
+        this.cards = [...this.originalCards];
+        console.log('Ordre croissant désactivé(nombre de clic pair)')
+      }
     } else if (order === 'desc') {
-      this.isClickedAsc = false;
-      this.isClickedDesc = true;
+      this.descClickCount++;
+      if(this.isClickedDesc = this.descClickCount % 2 !== 0) {
+        this.isClickedDesc = true;
+        this.isClickedAsc = false;
+        console.log('Ordre décroissant activé(nombre de clic impair)');
+      } else {
+        this.isClickedDesc = false;
+        this.cards = [...this.originalCards];
+        console.log('Ordre décroissant désactivé(nombre de clic pair)');
+      }
     }
   }
 
@@ -32,5 +54,6 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     this.cards = this.dataCardService.dataCard;
+    this.originalCards = [...this.cards]; // Stocke l'ordre original des cards
   }
 }
