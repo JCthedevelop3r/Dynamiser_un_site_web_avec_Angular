d4ef17672b876418a71e24aa37fc737ffea20d22
JIICE <minotjeanclaude60@gmail.com>
2024-06-12 18:26:40 +0100

    Add data for about page in existing service.

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataCardService } from '../data-card.service';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit{
+  aboutTitle: string = "";
+  aboutText1: string = "";
+  aboutText2: string = "";
+
+  constructor(private dataCardService: DataCardService) {}
+
+ngOnInit() {
+  this.aboutTitle = this.dataCardService.aboutTitle;
+  this.aboutText1 = this.dataCardService.aboutText1;
+  this.aboutText2 = this.dataCardService.aboutText2;
+}
 
 }
--- src/app/data-card.service.ts
+++ src/app/data-card.service.ts
@@ -65,4 +65,8 @@ export class DataCardService {
       price: "49,90€"
     }
   ]
+
+  aboutTitle = "Notre histoire";
+  aboutText1 = "Notre passion pour l'univers d'Astérix et Obélix nous a poussés à créer des figurines faites main, capturant l'esprit et les aventures de nos héros gaulois préférés. Chaque pièce est unique et réalisée avec amour.";
+  aboutText2 = "Fondée en 2022, notre entreprise réunit des artisans talentueux dédiés à la création de figurines de haute qualité. En puisant notre inspiration dans les aventures gauloises, nous souhaitons vous faire rêver avec nos créations."
 }
