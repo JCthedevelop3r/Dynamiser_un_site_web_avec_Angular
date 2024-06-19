fbf34aa13bf0ffc9b731d912faf48de05af30981
JIICE <minotjeanclaude60@gmail.com>
2024-06-19 23:06:54 +0100

    Allow header to be closed by clicking a link

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
 
+  ngOnInit(): void {
+    this.hideLinks();
+  }
+
+  hideLinks(): any {
+    let navBarCollapse = document.getElementById('navbarNav')
+    if(navBarCollapse !== null) {
+      navBarCollapse.classList.remove('show');
+    } else {
+      console.log('navbarToggler est null');
+    }
+  }
 }
