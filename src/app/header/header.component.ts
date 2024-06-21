57dc576cf3d160ddbb18515ff86bb7585f1d8d7b
JIICE <minotjeanclaude60@gmail.com>
2024-06-21 09:18:29 +0100

    Add an icon on the shopping bag

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,22 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent implements OnInit{
-
-  ngOnInit(): void {
-    
-  }
+export class HeaderComponent implements OnInit {
+  productShoppingBagCounter: number = 0;
 
   hideLinks(): any {
     let navBarCollapse = document.getElementById('navbarNav')
     if(navBarCollapse !== null) {
       navBarCollapse.classList.remove('show');
     } else {
-      console.log('navbarToggler est null');
+      console.log('property navbarToggler is null');
     }
+  }
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit(): void {
+    this.dataService.productShoppingBagCounter$.subscribe(counter => {
+      this.productShoppingBagCounter = counter;
+    });
   }
 }
